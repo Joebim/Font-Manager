5652cb9a9cdc5d0a74dac95d5e2338ce270e0ade
Joebim <josey359@gmail.com>
2023-01-03 04:40:23 +0100

    change fonts weight

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,11 +11,11 @@ export default function Sidebar(props) {
             <div className='w-full flex flex-col px-[10px] text-center items-center'>
                 <div className="h-[60px] w-[60px] my-[12px] text-white bg-black rounded-[90px] font-bold justify-center flex items-center">Fontu</div>
                 <div className="flex flex-col my-[25px] w-full justify-center">
-                    <div className=' text-[#575757] font-semibold hover:text-black mb-[10px] cursor-pointer'>Home</div>
-                    <div className=' text-[#575757] font-semibold hover:text-black mb-[10px] cursor-pointer'>App</div>
-                    <div className=' text-[#575757] font-semibold hover:text-black mb-[10px] cursor-pointer'>Pricing</div>
-                    <div className=' text-[#575757] font-semibold hover:text-black mb-[10px] cursor-pointer'>Partner</div>
-                    <div className=' text-[#575757] font-semibold hover:text-black mb-[10px] cursor-pointer'>AboutUs</div>
+                    <div className=' text-[#575757] font-medium hover:text-black mb-[10px] cursor-pointer'>Home</div>
+                    <div className=' text-[#575757] font-medium hover:text-black mb-[10px] cursor-pointer'>App</div>
+                    <div className=' text-[#575757] font-medium hover:text-black mb-[10px] cursor-pointer'>Pricing</div>
+                    <div className=' text-[#575757] font-medium hover:text-black mb-[10px] cursor-pointer'>Partner</div>
+                    <div className=' text-[#575757] font-medium hover:text-black mb-[10px] cursor-pointer'>AboutUs</div>
                 </div>
                 <button className='h-[40px] w-[100px] border-black border-solid border-[1px] rounded-[40px] text-center duration-[0.5s] text-black hover:text-white hover:bg-black'>Sign In</button>
             </div>
