28f98701a83b00185e2358c14f18900d3e255d4e
Joebim <josey359@gmail.com>
2023-01-03 04:21:34 +0100

    Change Text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Home() {
 
     const projectsData = [
-        "Product Design",
-        "Design Mockup",
-        "UI/UX",
-        "Develpment",
-        "Branding",
-        "Illustrations"
+        "Belotta",
+        "Righteous",
+        "Honey Cake",
+        "Balsamiq",
+        "Ubuntu",
+        "Georgia",
     ]
 
   return (
@@ -18,10 +18,10 @@ export default function Home() {
         </div>
         <div className="flex-[1]">
             <div className="w-[85%] mt-[100px] md:mt-[60px]">
-                <h1 className='text-[50px] leading-[55px]'>Manage All of Of Your Fonts In One Place Efficently</h1>
+                <h1 className='text-[50px] leading-[55px]'>Manage All Of Your Fonts In One Place Efficently</h1>
             </div>
             <div className="w-full md:w-[50%] my-[30px]">
-            <p className=''>Manage your work time lines and team mates all at once. Set the following timelines, assign tasks, and keep your projects in check</p>
+            <p className=''>Manage your font style, weight and size all at once. Set color to see the look, save and keep your fonts in check</p>
 
             </div>
             <div className="">
@@ -29,14 +29,14 @@ export default function Home() {
                 <button className='h-[40px] md:h-[60px] w-[100px] md:w-[150px] bg-purple-900 rounded-[40px] text-[14px] text-center duration-[0.5s] text-white'>Sign Up</button>
             </div>
 
-            <div className="mt-[100px] border-t-[1px] border-black flex flex-col md:flex-row py-[20px] mb-[50px]">
+            <div className="mt-[100px] border-t-[1px] border-black flex flex-col py-[20px] mb-[50px]">
                 <div className="flex-[1] mb-[20px]">
-                    <h1 className=' font-semibold'>TASKS AND PROJECTS</h1>
+                    <h1 className=' font-semibold'>SELECTED FONTS</h1>
                 </div>
                 <div className="flex-[2]">
                     {projectsData.map((project, index) => {
                         return (
-                            <button className='py-[6px] md:py-[11px] px-[20px] mb-[10px] mx-[6px] font-semibold text-[12px] border-black border-solid border-[1px] rounded-[40px] text-center duration-[0.5s] text-black hover:text-white hover:bg-black'>{project}</button>
+                            <button className='py-[6px] md:py-[11px] px-[16px] mb-[10px] mx-[6px] font-semibold text-[12px] border-black border-solid border-[1px] rounded-[40px] text-center duration-[0.5s] text-black hover:text-white hover:bg-black' key={index}>{project}</button>
                         )
                     })
                     }
